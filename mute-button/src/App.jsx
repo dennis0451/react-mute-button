948238cfc0bb6a3c12f165674a394cdb97190be4
Dennis Corral <dennis.corral@outlook.com>
2022-07-19 17:18:42 -0400

    done

---
+++ mute-button/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import React from 'react'
+import MuteButton from './containers/MuteButton'
+
+
+
+function App() {
+
+    
+  const [isMute, changeButton ]= useState(true)
+
+  const changeMute = () => {
+    if (isMute == true){
+      changeButton(false)
+    }else{
+      changeButton(true)
+    }
+    document.getElementById('answer').innerHTML=isMute
+  }
+  
+
+  return (
+    <div id='here' className="App">
+      <button onClick={changeMute}><MuteButton isTrue = {isMute}/></button>
+      <p id='answer'></p>
+
+    </div>
+  )
+}
+
+export default App
